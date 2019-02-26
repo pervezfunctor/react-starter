fa016c46f2c167a3ed9a8367932153b50be1cd0d
Pervez Iqbal <pervezfunctor@gmail.com>
2019-02-26 10:56:04 +0530

    drop mutative function

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-import { produce } from 'immer'
-
-type Mutable<T> = { -readonly [P in keyof T]: Mutable<T[P]> } // Remove readonly
-
-export const mutative = <T>(
-  obj: T,
-  f: (this: Mutable<T>, draft: Mutable<T>) => T | void,
-): T => produce(obj, f as any)
