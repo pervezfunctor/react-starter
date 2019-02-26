1da0e947313ac2041edc370e44e9daefd76cc302
Pervez Iqbal <pervezfunctor@gmail.com>
2019-02-26 10:57:13 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,6 @@ import React from 'react'
 import hot from 'react-hot-reload.macro'
 import { Hello } from './Hello'
 
-const App = () => <Hello />
+export const App = () => <Hello />
 
 export default hot(App)
