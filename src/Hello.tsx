3e53133303bd401bc16a9fcfe31a09dce34a0d09
Pervez Iqbal <pervezfunctor@gmail.com>
2019-03-16 16:15:35 +0530

    drop react-hot-reload

--- src/Hello.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-export interface HelloProps {
-  readonly name?: string
-}
-export const Hello: React.SFC<HelloProps> = ({ name }) => (
-  <h1>Hello, {name || 'World'}!</h1>
-)
